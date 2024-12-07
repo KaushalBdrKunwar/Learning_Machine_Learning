6adbcbe89525be0917d22dfa28b53a5ce3b42b60
KaushalBdrKunwar <kaushalbahadurkunwar@gmail.com>
2024-12-07 23:36:01 +0545

    Matrix manipulation using numpy

---
+++ NumPy/numpy_array_manpulation.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+array = np.random.randint(0,10,(2,3))
+print(array)
+print(array.shape)
+
+#Transpose of matrix.
+
+trans = np.transpose(array)
+print(trans)
+print(trans.shape)
+
